5c4cb4e9d62b417c30fb81f02f8356b5a8a30260
FeMotta <fernandosantosmotta@gmail.com>
2023-03-22 18:50:56 -0300

    adiciona estilo a pagina inicial

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
 import './App.scss';
-import Cabecalho from './cabecalho/Cabecalho';
 import Formulario from './formulario/Formulario';
 
 function App() {
   return (
     <div className="App">
-      {/* <Cabecalho /> */}
       <Formulario />
     </div>
   );
--- src/formulario/Formulario.jsx
+++ src/formulario/Formulario.jsx
@@ -1,36 +1,62 @@
-import { useState } from "react";
+import React, { useState } from 'react';
 import "./Formulario.scss";
 
 const Formulario = () => {
-  const [username, setUsername] = useState('');
-  const [password, setPassword] = useState('');
+  const [materia, setMateria] = useState('matematica');
+  const [nivel, setNivel] = useState('basico');
 
-  const handleUsernameChange = (event) => {
-    setUsername(event.target.value);
+  const handleMateriaChange = (event) => {
+    setMateria(event.target.value);
   };
 
-  const handlePasswordChange = (event) => {
-    setPassword(event.target.value);
+  const handleNivelChange = (event) => {
+    setNivel(event.target.value);
   };
 
   const handleSubmit = (event) => {
     event.preventDefault();
-    // Faça algo com os valores de usuário e senha aqui
-    console.log('Nome de usuário:', username);
-    console.log('Senha:', password);
+    console.log('Materia selecionada:', materia);
+    console.log('Nivel selecionado:', nivel);
+
   };
 
   return (
-    <div className="container">
-        <form className="neumorphic-form" onSubmit={handleSubmit}>
-        <label>Nome de usuário:</label>
-        <input type="text" name="username" value={username} onChange={handleUsernameChange} />
-        <label>Senha:</label>
-        <input type="password" name="password" value={password} onChange={handlePasswordChange} />
-        <input type="submit" value="Entrar" />
-        </form>
+    <div className="form-container">
+      <form className="edu-form" onSubmit={handleSubmit}>
+        <h2 className="form-title">    SmartAI    </h2>
+        <div className="form-group">
+          <select
+            name="materia"
+            id="materia-select"
+            className="form-select"
+            value={materia}
+            onChange={handleMateriaChange}
+          >
+            <option value="matematica">Matemática</option>
+            <option value="portugues">Português</option>
+            <option value="ciencias">Ciências</option>
+          </select>
+        </div>
+        <div className="form-group">
+          <select
+            name="nivel"
+            id="nivel-select"
+            className="form-select"
+            value={nivel}
+            onChange={handleNivelChange}
+          >
+            <option value="basico">Básico</option>
+            <option value="intermediario">Intermediário</option>
+            <option value="avancado">Avançado</option>
+          </select>
+        </div>
+        <button type="submit" className="form-button">
+          Confirmar
+        </button>
+      </form>
     </div>
   );
 };
+
 
 export default Formulario;
