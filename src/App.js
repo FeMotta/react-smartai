481b3f32b80490449efe12ec0df73241f69cb817
FeMotta <fernandosantosmotta@gmail.com>
2023-03-23 17:55:18 -0300

    adiciona serviço de Router

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.scss';
-import Formulario from './formulario/Formulario';
+import Formulario from './components/formulario/Formulario'
 
 function App() {
   return (
     <div className="App">
-      <Formulario />
+      <Router>
+        <Routes> 
+          <Route path="/" element={<Formulario/>} />
+        </Routes>
+      </Router>
     </div>
   );
 }
