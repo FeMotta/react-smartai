33ec9658a3e35659c5495227561e68c3d0567825
FeMotta <fernandosantosmotta@gmail.com>
2023-03-25 09:49:19 -0300

    refatora funcoes da QuestionPage

--- src/pages/QuestionPage/QuestionPage.jsx
+++ src/pages/QuestionPage/QuestionPage.jsx
@@ -1,63 +1,78 @@
-import React, { useState, useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-import { generateQuestions } from '../../services/api'
+import { generateQuestions, checkAnswer } from '../../services/api'
 
-import "./QuestionPage.scss";
 import Botao from '../../components/Botao/Botao';
 import Input from '../../components/Input/Input';
+import Spinner from '../../components/Spinner/Spinner';
+
+import "./QuestionPage.scss";
 
 const QuestionPage = () => {
+
+    // State
+
     const [answer, setAnswer] = useState('');
     const [question, setQuestion] = useState('');
-    const navigate = useNavigate();
+    const [loading, setLoading] = useState(false);
 
+    // Constants
+
+    const navigate = useNavigate();
+    
+    // Functions
+    
     const generateQuestion = async () => {
         const materia = localStorage.getItem('materia');
         const nivel = localStorage.getItem('nivel');
-
+        
         try {
             const question = await generateQuestions(materia, nivel);
             setQuestion(question);
             localStorage.setItem('question', question);
         } catch (error) {
             console.error(error);
+        } finally {
+            setLoading(false);
         }
     };
-
-    useEffect(() => {
-        generateQuestion();
-    }, []);
-
-    const changeQuestion = () => {
-        
-    };
-
-    // const storeAnswer = () => {
-    //     localStorage.setItem(`answer_${currentQuestionIndex}`, answer);
-    // };
   
     const handleChange = (event) => {
         setAnswer(event.target.value);
     };
   
-    const handleSubmit = (event) => {
+    const handleSubmit = async (event) => {
         event.preventDefault();
-        generateQuestion();
-        // storeAnswer();
-        changeQuestion();
-        setAnswer('');
-    };
 
+        setLoading(true);
+
+        await checkAnswer(question, answer).then( () => {
+            setAnswer('');
+            generateQuestion().then( () => {
+                setLoading(false);
+            });
+        })
+    };
 
     const backHome = (event) => {
         event.preventDefault();
+        
         navigate('/');
     };
+
+    useEffect(() => {
+        generateQuestion();
+        setLoading(true);
+    }, []);
   
     return (
         <div className="form-container" data-page="question-page">
             <form className='edu-form' onSubmit={handleSubmit}>
-                <h1 className="form-title">{question}</h1>
+                {loading ? (
+                    <Spinner />
+                ) : (
+                    <h1 className="form-title">{question}</h1>
+                )}
                 <Input
                     id="answer-input"
                     placeholder="Digite sua resposta"
