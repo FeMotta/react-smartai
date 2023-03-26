1786aae646221cb518260661887051c4d680e829
FeMotta <fernandosantosmotta@gmail.com>
2023-03-26 14:05:30 -0300

    altera cor spinner

--- src/components/Spinner/Spinner.jsx
+++ src/components/Spinner/Spinner.jsx
@@ -3,9 +3,9 @@ import React from 'react';
 const Spinner = () => {
     return (
         <div className="flex justify-center items-center h-full w-full pb-6">
-            <div className="w-4 h-4 mx-1 rounded-full bg-main animate-bounce duration-1400 ease-in-out"></div>
-            <div className="w-4 h-4 mx-1 rounded-full bg-main animate-bounce duration-1400 ease-in-out delay-320"></div>
-            <div className="w-4 h-4 mx-1 rounded-full bg-main animate-bounce duration-1400 ease-in-out delay-160"></div>
+            <div className="w-4 h-4 mx-1 rounded-full bg-secondary animate-bounce duration-1400 ease-in-out"></div>
+            <div className="w-4 h-4 mx-1 rounded-full bg-secondary animate-bounce duration-1400 ease-in-out delay-320"></div>
+            <div className="w-4 h-4 mx-1 rounded-full bg-secondary animate-bounce duration-1400 ease-in-out delay-160"></div>
         </div>
     );
 };
