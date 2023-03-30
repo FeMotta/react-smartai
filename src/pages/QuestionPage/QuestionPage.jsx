6a63d9f3a10b41567730b07efe58a307fa335c98
FeMotta <fernandosantosmotta@gmail.com>
2023-03-30 20:49:38 -0300

    remove função Contador

--- src/pages/QuestionPage/QuestionPage.jsx
+++ src/pages/QuestionPage/QuestionPage.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState, Component } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { generateQuestions, checkAnswer } from '../../services/api'
 
@@ -11,111 +11,100 @@ import Heading from '../../components/Heading/Heading';
 
 const QuestionPage = () => {
 
-    // State
+  // State
 
-    const [answer, setAnswer] = useState('');
-    const [question, setQuestion] = useState('');
-    const [loading, setLoading] = useState(false);
-    const [count, setCount] = useState(1);
+  const [answer, setAnswer] = useState('');
+  const [question, setQuestion] = useState('');
+  const [loading, setLoading] = useState(false);
+  const [count, setCount] = useState(1);
 
-    // Constants
+  // Constants
 
-    const navigate = useNavigate();
+  const navigate = useNavigate();
+  
+  // Functions
+  
+  const generateQuestion = async () => {
+    const materia = localStorage.getItem('materia');
+    const nivel = localStorage.getItem('nivel');
     
-    // Functions
-    
-    const generateQuestion = async () => {
-        const materia = localStorage.getItem('materia');
-        const nivel = localStorage.getItem('nivel');
-        
-        try {
-            const question = await generateQuestions(materia, nivel);
-            setQuestion(question);
-            localStorage.setItem('question', question);
-        } catch (error) {
-            console.error(error);
-        } finally {
+    try {
+      const question = await generateQuestions(materia, nivel);
+      setQuestion(question);
+      localStorage.setItem('question', question);
+    } catch (error) {
+      console.error(error);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  const handleChange = (event) => {
+    setAnswer(event.target.value);
+  };
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+
+    if (answer !== '') {
+      setLoading(true);
+
+      await checkAnswer(question, answer).then( () => {
+        setAnswer('');
+          generateQuestion().then( () => {
             setLoading(false);
-        }
-    };
+            setCount(count + 1); 
+        });
+      })
+    }
+  };
+
+  const backHome = (event) => {
+    event.preventDefault();
+    
+    navigate('/');
+  };
+
+  useEffect(() => {
+    generateQuestion();
+    setLoading(true);
+  }, []);
   
-    const handleChange = (event) => {
-        setAnswer(event.target.value);
-    };
-  
-    const handleSubmit = async (event) => {
-        event.preventDefault();
-
-        if (answer !== '') {
-            setLoading(true);
-    
-            await checkAnswer(question, answer).then( () => {
-                setAnswer('');
-                generateQuestion().then( () => {
-                    setLoading(false);
-                    setCount(count + 1); 
-                });
-            })
-        }
-    };
-
-    const backHome = (event) => {
-        event.preventDefault();
-        
-        navigate('/');
-    };
-
-    useEffect(() => {
-        generateQuestion();
-        setLoading(true);
-    }, []);
-    
-    const Contador = () => {
-        return (
-            <div className="flex justify-center mb-4">
-            <p className="text-white-500 font-semibold">{count}/10</p>
-          </div>
-        );
-    };
-    
-    return (
-        <>
-        <Contador count={count} formClassName="lg:min-w-600 p-5"/>
-        <Container>
-            <Formulario formClassName="lg:min-w-600 p-5" onSubmit={handleSubmit}>
-                {loading ? (
-                    <Spinner />
-                    ) : (
-                        <Heading headingClassName="w-full">{question}</Heading>
-                        )}
-                <Input
-                    id="answer-input"
-                    placeholder="Digite sua resposta"
-                    value={answer}
-                    onChange={handleChange}
-                    inputClassName={`border-2 rounded-md px-4 py-2 ${
-                        answer === '' ? 'border-red-500' : 'border-primary'
-                    }`}
-                />
-                <div className="flex justify-between items-center w-full">
-                    <Botao
-                        buttonClassName="form-button flex-grow mx-2 first:ml-0 last:mr-0"
-                        onClick={backHome}
-                    >
-                        Voltar para Home
-                    </Botao>
-                    <Botao
-                        buttonClassName="form-button flex-grow mx-2 first:ml-0 last:mr-0"
-                        disabled={answer === ''}
-                        onClick={handleSubmit}
-                    >
-                        Próxima Questão
-                    </Botao>
-                </div>
-            </Formulario>
-        </Container>
-        </>
-    );
+  return (
+    <Container>
+      <div className="flex justify-center mb-1">
+        <p className="text-white-500 font-semibold">{count}/10</p>
+      </div>
+      <Formulario formClassName="lg:min-w-600 p-5" onSubmit={handleSubmit}>
+        {loading ? (
+          <Spinner />
+        ) : (
+          <Heading headingClassName="w-full">{question}</Heading>
+        )}
+        <Input
+          id="answer-input"
+          placeholder="Digite sua resposta"
+          value={answer}
+          onChange={handleChange}
+          inputClassName={`border-2 rounded-md px-4 py-2 ${answer === '' ? 'border-red-500' : 'border-primary'}`} />
+        <div className="flex justify-between items-center w-full">
+          <Botao
+            buttonClassName="form-button flex-grow mx-2 first:ml-0 last:mr-0"
+            onClick={backHome}
+          >
+            Voltar para Home
+          </Botao>
+          <Botao
+            buttonClassName="form-button flex-grow mx-2 first:ml-0 last:mr-0"
+            disabled={answer === ''}
+            onClick={handleSubmit}
+          >
+            Próxima Questão
+          </Botao>
+        </div>
+      </Formulario>
+    </Container>
+  );
 };
   
 export default QuestionPage;
