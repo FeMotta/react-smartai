8805802e890cb254e9b1b9e500c851766a5207c9
FeMotta <fernandosantosmotta@gmail.com>
2023-04-22 20:57:42 -0300

    altera url do axios

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const apiClient = axios.create({
-    baseURL: 'http://192.168.15.12:3001',
+    baseURL: 'http://localhost:3001',
     headers: {
         'Content-Type': 'application/json',
     },
