52ee6b32f10245c60ef2f6ae17765dc9a5136bef
FeMotta <fernandosantosmotta@gmail.com>
2023-04-25 14:24:43 -0300

    adiciona input para chave de api

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,16 +1,27 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
+import Cookies from 'js-cookie';
 
 import Botao from '../components/Botao';
 import Formulario from '../components/Formulario';
 import Container from '../components/Container';
 import Seletor from '../components/Seletor';
 import Heading from '../components/Heading';
+import SecureInput from '../components/SecureInput';
 
 const Home = () => {
+
+  // States
+
   const [materia, setMateria] = useState('matematica');
   const [nivel, setNivel] = useState('basico');
+  const [apikey, setApikey] = useState('');
+
+  // Constants
+
   const navigate = useNavigate();
+
+  // Functions
 
   const handleMateriaChange = (event) => { 
     setMateria(event.target.value);
@@ -20,11 +31,18 @@ const Home = () => {
     setNivel(event.target.value);
   };
 
+  const handleApiKeyChange = (event) => {
+    setApikey(event.target.value);
+  };
+
   const handleSubmit = (e) => {
     e.preventDefault();
 
     localStorage.setItem('materia', materia);
     localStorage.setItem('nivel', nivel);
+
+    Cookies.set('api_key', apikey, { secure: false, sameSite: 'strict' });
+
     navigate('/question-page');
   };
 
@@ -55,6 +73,13 @@ const Home = () => {
             { label: 'Avançado', value: 'avancado' },
           ]}
           onChange={handleNivelChange}
+        />
+        <SecureInput
+          id="apikey-input"
+          placeholder="Insira sua chave de API"
+          isPassword={true}
+          value={apikey}
+          onChange={handleApiKeyChange}
         />
         <Botao 
           onClick={handleSubmit}
